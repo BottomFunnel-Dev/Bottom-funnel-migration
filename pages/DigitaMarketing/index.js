cc2fc9f82a8de162d3b9f3e3309464b9d6182401
1avinash2023 <118244986+1avinash2023@users.noreply.github.com>
2023-01-12 15:36:42 +0530

    added

--- pages/DigitaMarketing/index.js
+++ pages/DigitaMarketing/index.js
@@ -6,16 +6,6 @@ import { DigitalmarketWhyConsider } from "./DigitalmarketWhyConsider";
 import { DigitaMarketWeOffers } from "./DigitaMarketWeOffers";
 import { WhatDigitalMarket } from "./WhatDigitalMarket";
 
-import StoriesSection from "../CommonComponents/StoriesSection/StoriesSection";
-import { Industries } from "../CommonComponents/industries/Industries";
-import PlanProject from "../CommonComponents/PlanProject/PlanProject";
-import OndemandSection from "../CommonComponents/OnDemandSection/OndemandSection";
-import PortFolio from "../CommonComponents/PortFolioSection/PortFolio";
-import { SeoblogSection } from "../CommonComponents/SeoBlogs/Seoblogs";
-import Revamping from "../CommonComponents/revamping/Revamping";
-import { TextDropdown } from "../CommonComponents/faqSection/Faqs";
-import ContectForm from "../CommonComponents/ContectForm/ContectForm";
-import MainFooter from "../CommonComponents/FooterSection/MainFooter";
 
 export default function DigitalMarketing() {
   return (
@@ -23,20 +13,12 @@ export default function DigitalMarketing() {
       <DigitalMarketingBanner />
       <WhatDigitalMarket />
       <DigitaMarketWeOffers />
-      <DigitalMarketingHiring />
+         {/* <DigitalMarketingHiring />
       <DigitalMarketClientsAdd />
       <DigitalmarketWhyConsider />
 
-      <StoriesSection />
-      <Industries />
-      <PlanProject />
-      <OndemandSection />
-      {/* <PortFolio /> */}
-      <SeoblogSection />
-      <Revamping />
-      <TextDropdown />
-      <ContectForm />
-      {/* <MainFooter /> */}
+  */}
+     
     </div>
   );
 }
