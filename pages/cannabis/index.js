cbffabf4887c7d2de58cfc45b390c1b0f4669d71
Akshay-Kumar2000 <akshaykumar809177@gmail.com>
2023-01-18 12:00:39 +0530

    code migration

---
+++ pages/cannabis/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import Cannabisoverview from "../../components/Cannabis/Cannabisoverview";
+import Cannabissolution from "../../components/Cannabis/Cannabissolution";
+import { Cannabiscontainer } from "../../components/Cannabis/Cannabiscontainer";
+import { Cannabiscustomersection } from "../../components/Cannabis/Cannabiscustomersection";
+import { Cannabisadmin } from "../../components/Cannabis/Cannabisadmin";
+import { Cannabisdrive } from "../../components/Cannabis/Cannabisdrive";
+
+import { TextDropdown } from "../../components/CommonComponents/faqSection/Faqs";
+import OndemandSection from "../../components/CommonComponents/OnDemandSection/OndemandSection";
+import PlanProject from "../../components/CommonComponents/PlanProject/PlanProject";
+import { SeoblogSection } from "../../components/CommonComponents/SeoBlogs/Seoblogs";
+import StoriesSection from "../../components/CommonComponents/StoriesSection/StoriesSection";
+import Revamping from "../../components/revamping/Revamping";
+import ContectForm from "../../components/CommonComponents/ContectForm/ContectForm";
+import MainFooter from "../../components/CommonComponents/FooterSection/MainFooter";
+
+export default function Cannabis() {
+  return (
+    <div>
+      <Cannabiscontainer />
+      <Cannabissolution />
+      <Cannabisoverview />
+      <Cannabiscustomersection />
+      <Cannabisadmin />
+      <Cannabisdrive />
+
+      <StoriesSection />
+      {/* <Industries /> */}
+      <PlanProject />
+      <OndemandSection />
+      {/* <PortFolio /> */}
+      <SeoblogSection />
+      <Revamping />
+      <TextDropdown />
+      <ContectForm />
+      <MainFooter />
+    </div>
+  );
+}
