884c98319107f0fc177be85dd1c96b76c6b4a919
Akshay-Kumar2000 <akshaykumar809177@gmail.com>
2023-01-24 16:42:29 +0530

    code changes

--- next.config.js
+++ next.config.js
@@ -1,29 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   distDir: "build",
-// };
-
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   distDir: "build",
-  beforeFiles: [
-    // if the host is `acme.com`,
-    // this rewrite will be applied
-    {
-      source: "/:php-development*",
-      has: [
-        {
-          type: "host",
-          value: "bottomfunnel.net",
-        },
-      ],
-      destination: "sample.bottomfunnel.net/:php-development*",
-    },
-  ],
 };
 
 module.exports = nextConfig;
