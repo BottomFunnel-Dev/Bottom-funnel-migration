3505856f61829af762cb6d806e6859967388ea18
hemantjayas <hemantjayas03@gmail.com>
2023-01-25 12:23:36 +0530

    Revert "chnages"
    This reverts commit 20df193e19bc4ff489b083fcb167acabb5dd8d7c.

--- pages/alcoholdeliveryapp/index.js
+++ pages/alcoholdeliveryapp/index.js
@@ -20,7 +20,15 @@ import Navbar from "../../components/Navbar/Navbar";
 export default function AlcoholDelivery() {
   return (
     <div>
-      <Navbar />
+ <Navbar
+        productMount={{
+          navMount: false,
+        }}
+        navredux={{
+          color: "white",
+          logo: true,
+        }}
+      />
       <AlcoholBanner />
       <Trusted partnerLogo={alcoholLogo} />
       <AlcoholBookCall />
