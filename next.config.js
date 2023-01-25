8911f96886960e81dd49d5511063813189c5aef0
hemantjayas <hemantjayas03@gmail.com>
2023-01-25 12:31:39 +0530

    chanhes in config

--- next.config.js
+++ next.config.js
@@ -2,20 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   distDir: "build",
-  beforeFiles: [
-    // if the host is `acme.com`,
-    // this rewrite will be applied
-    {
-        source: '/:php-development*',
-        has: [
-            {
-                type: 'host',
-                value: 'acme.com',
-            },
-        ],
-        destination: 'app.acme.com/:php-development*',
-    },
-]
+  
+
 };
 
 module.exports = nextConfig;
