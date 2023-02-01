58eb45d582bcd39faf6568b8f419d843ed8bc097
hemantjayas <hemantjayas03@gmail.com>
2023-02-01 20:29:47 +0530

    chanhesd

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import '../styles/globals.css'
 import "slick-carousel/slick/slick.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 import { store } from '../redux/store';
 import { Provider } from 'react-redux';
 export default function App({ Component, pageProps }) {
   return <Provider store={store}>
     <Component {...pageProps} />
-    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"/>
   </Provider>
 }
   
