75fe91a51341a74b47784d6e94dbcbca51bcc98f
hemantjayas <hemantjayas03@gmail.com>
2023-02-03 12:56:00 +0530

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import '../styles/globals.css'
 import "slick-carousel/slick/slick.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { SSRProvider } from 'react-bootstrap';
 
+import { store } from '../redux/store';
+import { Provider } from 'react-redux';
 export default function App({ Component, pageProps }) {
-  return 
+  return <SSRProvider store={store}>
     <Component {...pageProps} />
-  
+  </SSRProvider>
 }
   
