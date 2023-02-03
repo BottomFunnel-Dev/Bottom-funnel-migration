64853e308e4ec221550764ae3aa86244a69babd2
hemantjayas <hemantjayas03@gmail.com>
2023-02-03 10:28:53 +0530

    first commit of the day

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import '../styles/globals.css'
 import "slick-carousel/slick/slick.css";
-import 'bootstrap/dist/css/bootstrap.css';
-import { useEffect } from 'react';
+// import 'bootstrap/dist/css/bootstrap.css';
 
 import SSRProvider from 'react-bootstrap/SSRProvider';
 export default function App({ Component, pageProps }) {
 
-  useEffect(() => {
-    import ('bootstrap/dist/js/bootstrap.js')
-}, []);
+
 
 
 
