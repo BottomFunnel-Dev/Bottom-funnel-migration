d3cef9a4f3426f299201f965f761ea84298a2523
hemantjayas <hemantjayas03@gmail.com>
2023-02-03 11:00:10 +0530

    changs

--- pages/apply-as-talent/index.js
+++ pages/apply-as-talent/index.js
@@ -5,14 +5,9 @@ import Head from "next/head";
 export default function () {
   return (
     <div>
-      {/* <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-          crossorigin="anonymous"
-        />
-      </Head> */}
+      <Head>
+       <title>apply as talent</title>
+      </Head>
       <ApplyNavbar />
       <Banner />
     </div>
