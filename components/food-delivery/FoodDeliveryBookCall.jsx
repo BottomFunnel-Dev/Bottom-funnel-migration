79b4592e4aaf14bf6242e2f82e76462c81d26f71
Akshay-Kumar2000 <akshaykumar809177@gmail.com>
2023-02-08 13:13:49 +0530

    css changes

--- components/food-delivery/FoodDeliveryBookCall.jsx
+++ components/food-delivery/FoodDeliveryBookCall.jsx
@@ -4,8 +4,8 @@ import styles from "./FoodDeliveryBookCall.module.css";
 export const FoodDeliveryBookCall = () => {
   return (
     <div className={styles.foodDeliveryBookCallMain}>
-      <h2>Discuss your requirements with our experts</h2>
-      <button>Book a call with our experts</button>
+      <h2> Discuss your requirements with our experts </h2>
+      <button> Book a call with our experts </button>
     </div>
   );
 };
