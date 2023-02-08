6d7662752068f92b88f14efc01336665682d1fb4
Deepanshu Gulia <deepanshugulia256@gmail.com>
2023-02-08 16:17:45 +0530

    done with discover page

--- components/ProductDiscovery/DreamProject/DreamProject.jsx
+++ components/ProductDiscovery/DreamProject/DreamProject.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React from "react";
 import styles from "./DreamProject.module.css";
 
 export const DreamProject = () => {
   return (
     <div className={styles.DreamProjectmaindiv}>
-        <div className={styles.DreamProjectcontain} >
-            <h1>Kickstart Your Dream Project With Us </h1>
-            <p>We have worked with some of the best innovation ideas and brands in the world across industries.</p>
-
-        </div>
-        <div>
-            <button className={styles.DreamProjectbutton}>Initiate a Partnership</button>
-        </div>
+      <div className={styles.DreamProjectcontain}>
+        <h1>Kickstart Your Dream Project With Us </h1>
+        <p>
+          We have worked with some of the best innovation ideas and brands in
+          the world across industries.
+        </p>
+      </div>
+      <button className={styles.DreamProjectbutton}>
+        Initiate a Partnership
+      </button>
     </div>
-  )
-}
+  );
+};
--- components/ProductDiscovery/ProductDiscoveryBanner/ProductDiscoveryBanner.jsx
+++ components/ProductDiscovery/ProductDiscoveryBanner/ProductDiscoveryBanner.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 import styles from "./ProductDiscoveryBanner.module.css";
 
 export const ProductDiscoveryBanner = () => {
   return (
     <div>
-        <div className={styles.ProductDiscoveryBannermaindiv}>
-          <div className={styles.ProductDiscoveryBannercontain} >
-            <h1><span>Product Design</span> Sprint</h1>
-            <p>Product design sprint is to evaluate and prioritize the core features of your product. Sprint is a time boxed exercise,
-               where we'll act quickly to explore how to create a better user experience for your product.</p>
-               <div>
-                <button className={styles.ProductDiscoveryBannerbutton}>Get Started</button>
-               </div>
-
+      <div className={styles.ProductDiscoveryBannermaindiv}>
+        <div className={styles.ProductDiscoveryBannercontain}>
+          <h1>
+            <span>Product Design</span> Sprint
+          </h1>
+          <p>
+            Product design sprint is to evaluate and prioritize the core
+            features of your product. Sprint is a time boxed exercise, where
+            we'll act quickly to explore how to create a better user experience
+            for your product.
+          </p>
+          <div>
+            <button className={styles.ProductDiscoveryBannerbutton}>
+              Get Started
+            </button>
           </div>
-          <div className={styles.ProductDiscoveryBannerimage}>
-            <img src="/Images/Productdiscoveryphotos/productbannerg.png" alt="Bannerimage "  />
-          </div>
-            
         </div>
+        <div className={styles.ProductDiscoveryBannerimage}>
+          <Image
+            width={"0"}
+            height={"0"}
+            sizes={"100vw"}
+            style={{ width: "100%", height: "100%" }}
+            src="/Images/Productdiscoveryphotos/productbannerg.webp"
+            alt="Productdiscoveryphotos/productbannerg.webp "
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
