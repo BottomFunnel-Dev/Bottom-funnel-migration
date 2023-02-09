d8726406afc85f3b54966662b337d63c5e1b2a01
1avinash2023 <118244986+1avinash2023@users.noreply.github.com>
2023-02-09 15:45:41 +0530

    added

--- components/Beautyandcare/Beautycontainer.jsx
+++ components/Beautyandcare/Beautycontainer.jsx
@@ -5,30 +5,29 @@ import Lottie from "react-lottie";
 
 export default function Beautycontainer() {
 
-  const lottieDefaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: reactAnimation,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  };
+const lottieDefaultOptions = {
+loop: true,
+autoplay: true,
+animationData: reactAnimation,
+rendererSettings: {
+preserveAspectRatio: "xMidYMid slice"
+}
+};
 
-  return (
-    <div className={styles.beautycontainer}>
-      <div className={styles.beautyleft}>
-        <h1 className={styles.heading}>Beauty And Salon App Development Solutions</h1>
-        <p>
-          Beautify Your Business with An App! Enhance your salon's customer
-          experience with our cutting-edge beauty & salon app development.
-        </p>
-        <button> Get Started</button>
-      </div>
+return (
+<div className={styles.beautycontainer}>
+<div className={styles.beautyleft}>
+<h1 className={styles.heading}>Beauty And Salon App Development Solutions</h1>
+<p>
+Beautify Your Business with An App! Enhance your salon's customer
+experience with our cutting-edge beauty & salon app development.
+</p>
+<button> Get Started</button>
+</div>
 
-      <div className={styles.mobile}>
-        <Lottie options={lottieDefaultOptions} width={700} height={650} />
-        {/* <img src="/Images/beautycare/beautymobile.png" alt="image" /> */}
-      </div>
-    </div>
-  );
+<div className={styles.mobile}>
+<Lottie options={lottieDefaultOptions} width={700} height={650} />
+</div>
+</div>
+);
 }
