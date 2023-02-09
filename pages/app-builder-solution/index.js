63c13fc6b9cf8e4cb19af28d69664b543b89e9fb
1avinash2023 <118244986+1avinash2023@users.noreply.github.com>
2023-02-09 12:59:12 +0530

    added

--- pages/app-builder-solution/index.js
+++ pages/app-builder-solution/index.js
@@ -1,11 +1,9 @@
 import React from "react";
-
 import { AppBuilderSolutionBanner } from "../../components/AppBuilderSoltion/AppBuilderSolutionBanner";
 import { AppBuilderDiscoverApp } from "../../components/AppBuilderSoltion/AppBuilderDiscoverApp";
 import { AppBuilderWhyShuld } from "../../components/AppBuilderSoltion/AppBuilderWhyShuld";
 import { AppBuilderCreate } from "../../components/AppBuilderSoltion/AppBuilderCreate";
 import { AppBuilderMaker } from "../../components/AppBuilderSoltion/AppBuilderMaker";
-
 import { TextDropdown } from "../../components/CommonComponents/faqSection/Faqs";
 import OndemandSection from "../../components/CommonComponents/OnDemandSection/OndemandSection";
 import PlanProject from "../../components/CommonComponents/PlanProject/PlanProject";
@@ -16,50 +14,47 @@ import ContectForm from "../../components/CommonComponents/ContectForm/ContectFo
 import MainFooter from "../../components/CommonComponents/FooterSection/MainFooter";
 import Navbar from "../../components/Navbar/Navbar";
 import Head from "next/head";
-
 export default function AppBuilderSolution () {
-  return (
-    <div>
+return (
+<div>
+<Head>
+<title>App Builder Solutions Company | Bottom Funnel</title>
+<meta
+name="description"
+content="Wondering how to develop an app? Look no further, we can 
+help! We specialize in providing top-notch App Builder 
+Solutions Development services that will help you turn your
+idea into a reality."
+/>
+</Head>
+<main>
+<Navbar
+productMount={{
+navMount: false,
+}}
+navredux={{
+color: "black",
+logo: false,
+}}
+/>
+<AppBuilderSolutionBanner />
+<AppBuilderDiscoverApp />
+<AppBuilderWhyShuld />
+<AppBuilderCreate />
+<AppBuilderMaker />
 
-      <Head>
-        <title>App Builder Solutions Company | Bottom Funnel</title>
-        <meta
-          name="description"
-          content="Wondering how to develop an app? Look no further, we can 
-          help! We specialize in providing top-notch App Builder 
-          Solutions Development services that will help you turn your
-          idea into a reality."
-        />
-      </Head>
-      <main>
+{/* Other react required components*/}
+<StoriesSection />
+<OndemandSection />
+{/* <PortFolio />*/}
+<PlanProject />
+<SeoblogSection />
+<Revamping />
+<TextDropdown />
+<ContectForm />
+<MainFooter />
 
-      <Navbar
-        productMount={{
-          navMount: false,
-        }}
-        navredux={{
-          color: "black",
-          logo: false,
-        }}
-      />
-      <AppBuilderSolutionBanner />
-      <AppBuilderDiscoverApp />
-      <AppBuilderWhyShuld />
-      <AppBuilderCreate />
-      <AppBuilderMaker />
-      
-      {/* Other react required components  */}
-      <StoriesSection />
-      <OndemandSection />
-      {/* <PortFolio />  */}
-      <PlanProject />
-      <SeoblogSection />
-      <Revamping />
-      <TextDropdown />
-      <ContectForm />
-      <MainFooter />
-
-      </main>
-    </div>
-  );
+</main>
+</div>
+);
 };
