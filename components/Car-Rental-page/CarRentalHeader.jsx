d09bc2d44a22852e54ee7a56995c15d2e377c7c2
1avinash2023 <118244986+1avinash2023@users.noreply.github.com>
2023-02-09 17:35:37 +0530

    added

--- components/Car-Rental-page/CarRentalHeader.jsx
+++ components/Car-Rental-page/CarRentalHeader.jsx
@@ -4,28 +4,27 @@ import * as reactAnimation from "../../public/Animation/SolutionsAnimation/car r
 import Lottie from "react-lottie";
 
 export const CarRentalHeader = () => {
+const lottieDefaultOptions = {
+loop: true,
+autoplay: true,
+animationData: reactAnimation,
+rendererSettings: {
+preserveAspectRatio: "xMidYMid slice"
+}
+};
 
-  const lottieDefaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: reactAnimation,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  };
+return (
+<div className={styles.RGcarRentalContainer}>
+<div className={styles.RGcarRentalHeaderLeft}>
+<h1> Car Rental App Solution</h1>
+<p>The Screeno ecosystem is designed to help you generate profit. Set up complete sales and marketing funnels with ease using the Screeno</p>
+<button>Start Free Trail </button>
+</div>
 
-  return (
-    <div className={styles.RGcarRentalContainer}>
-      <div className={styles.RGcarRentalHeaderLeft}>
-        <h1> Car Rental App Solution</h1>
-        <p>The Screeno ecosystem is designed to help you generate profit. Set up complete sales and marketing funnels with ease using the Screeno</p>
-        <button>Start Free Trail </button>
-      </div>
+<div className={styles.RGcarRentalHeaderRight}>
+<Lottie options={lottieDefaultOptions} width={600} height={600} />
 
-      <div className={styles.RGcarRentalHeaderRight}>
-        <Lottie options={lottieDefaultOptions} width={600} height={600} />
-        {/* <imgs width="100%" src="Images/Car-Rental-Page-Images/Banner1.png" alt="BannerImage" /> */}
-      </div>
-    </div>
-  );
+</div>
+</div>
+);
 };
--- pages/car-rentals-app/index.js
+++ pages/car-rentals-app/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { CarRentalHeader } from "../../components/Car-Rental-page/CarRentalHeader";
 import { FeaturesComp } from "../../components/Car-Rental-page/FeaturesComp";
 import { GreyBoxComponent } from "../../components/Car-Rental-page/GreyBoxComponent";
@@ -8,7 +7,6 @@ import { AdminPannelComp } from "../../components/Car-Rental-page/AdminPannelCom
 import { CustomerAppComp } from "../../components/Car-Rental-page/CustomerAppComp";
 import { ChooseUsComp } from "../../components/Car-Rental-page/ChooseUsComp";
 import { CarRentalFooter } from "../../components/Car-Rental-page/CarRentalFooter";
-
 import { TextDropdown } from "../../components/CommonComponents/faqSection/Faqs";
 import OndemandSection from "../../components/CommonComponents/OnDemandSection/OndemandSection";
 import PlanProject from "../../components/CommonComponents/PlanProject/PlanProject";
@@ -23,48 +21,48 @@ import Head from "next/head";
 export default function CarRentals() {
   return (
     <>
-      <Head>
-        <title> Car Rentals Web App Development Company | Bottom Funnel </title>
-        <meta
-          name="description"
-          content="Wondering how to start your own car rental web app? 
-        Don't worry, we can help! Our web app development company 
-        has years of experience in developing car rental web apps for 
-        various companies."
-        />
-      </Head>
+<Head>
+  <title> Car Rentals Web App Development Company | Bottom Funnel </title>
+  <meta
+    name="description"
+    content="Wondering how to start your own car rental web app? 
+  Don't worry, we can help! Our web app development company 
+  has years of experience in developing car rental web apps for 
+  various companies."
+  />
+</Head>
 
-      <main>
-        <Navbar
-          productMount={{
-            navMount: false,
-          }}
-          navredux={{
-            color: "black",
-            logo: false,
-          }}
-        />
+<main>
+  <Navbar
+    productMount={{
+navMount: false,
+    }}
+    navredux={{
+color: "black",
+logo: false,
+    }}
+  />
 
-        <CarRentalHeader />
-        <FeaturesComp />
-        <GreyBoxComponent />
-        <RenterAppComp />
-        <AdminPannelComp />
-        <CustomerAppComp />
-        <ChooseUsComp />
-        {/* <CarRentalFooter /> */}
+  <CarRentalHeader />
+  <FeaturesComp />
+  <GreyBoxComponent />
+  <RenterAppComp />
+  <AdminPannelComp />
+  <CustomerAppComp />
+  <ChooseUsComp />
+  {/* <CarRentalFooter /> */}
 
-        <StoriesSection />
-        {/* <Industries /> */}
-        <PlanProject />
-        <OndemandSection />
-        {/* <PortFolio /> */}
-        <SeoblogSection />
-        <Revamping />
-        <TextDropdown />
-        <ContectForm />
-        <MainFooter />
-      </main>
+  <StoriesSection />
+  {/* <Industries /> */}
+  <PlanProject />
+  <OndemandSection />
+  {/* <PortFolio /> */}
+  <SeoblogSection />
+  <Revamping />
+  <TextDropdown />
+  <ContectForm />
+  <MainFooter />
+</main>
     </>
   );
 }
