c646d0ab0b7c3b34df9afc6ad700078cdeceb27f
Akshay-Kumar2000 <akshaykumar809177@gmail.com>
2023-02-13 12:26:06 +0530

    added compressed images

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,11 @@ const Awards = dynamic(() => import("../components/AwardSection/Awards"), {
 });
 const TextDropdown = dynamic(
   () => import("../components/CommonComponents/faqSection/Faqs"),
-  { loading: () => "Loading..." },
+  { loading: () => "Loading..." }
 );
 const MainFooter = dynamic(
   () => import("../components/CommonComponents/FooterSection/MainFooter"),
-  { loading: () => "Loading..." },
+  { loading: () => "Loading..." }
 );
 const Services = dynamic(() => import("../components/Services/Services"), {
   loading: "Loading...",
@@ -23,45 +23,45 @@ const Services = dynamic(() => import("../components/Services/Services"), {
 const OndemandSection = dynamic(
   () =>
     import("../components/CommonComponents/OnDemandSection/OndemandSection"),
-  { loading: "Loading..." },
+  { loading: "Loading..." }
 );
 const PlanProject = dynamic(
   () => import("../components/CommonComponents/PlanProject/PlanProject"),
-  { loading: "Loading..." },
+  { loading: "Loading..." }
 );
 const PortFolio = dynamic(
   () => import("../components/CommonComponents/PortFolioSection/PortFolio"),
-  { loading: "Loading..." },
+  { loading: "Loading..." }
 );
 const Revamping = dynamic(
   () => import("../components/CommonComponents/revamping/Revamping"),
-  { loading: "Loading..." },
+  { loading: "Loading..." }
 );
 const SeoblogSection = dynamic(
   () => import("../components/CommonComponents/SeoBlogs/Seoblogs"),
-  { loading: "Loading..." },
+  { loading: "Loading..." }
 );
 const StoriesSection = dynamic(
   () => import("../components/CommonComponents/StoriesSection/StoriesSection"),
-  { loading: "Loading..." },
+  { loading: "Loading..." }
 );
 const CompanyLogo = dynamic(
   () => import("../components/CompanyLogos/CompanyLogo"),
-  { loading: "Loading..." },
+  { loading: "Loading..." }
 );
 const Companystats = dynamic(
   () => import("../components/CompanyStats/Companystats"),
-  { loading: "Loading..." },
+  { loading: "Loading..." }
 );
 const ContectForm = dynamic(
   () => import("../components/ContectForm/ContectForm"),
-  { loading: "Loading..." },
+  { loading: "Loading..." }
 );
 const BannerFull = dynamic(
   () => import("../components/mainSection/BannerFull"),
   {
     loading: () => "Loading...",
-  },
+  }
 );
 
 export default function Home() {
