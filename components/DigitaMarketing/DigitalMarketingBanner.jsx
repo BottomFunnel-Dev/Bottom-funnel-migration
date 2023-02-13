1fd127e01296981f1ea69b565f4387eb1ee9e337
jyotiranjan98 <jyoti.das955@gmail.com>
2023-02-13 16:14:27 +0530

    changes

--- components/DigitaMarketing/DigitalMarketingBanner.jsx
+++ components/DigitaMarketing/DigitalMarketingBanner.jsx
@@ -5,7 +5,7 @@ export const DigitalMarketingBanner = () => {
   return (
     <div className={styles.digitalmarketingbannermainboxes}>
       <div className={styles.digitalmarketingbannerspacefortext1}>
-        <h1>Creative Digital Marketing Agency</h1>
+        <h1>Creative Digital Marketing Agency </h1>
         <p>
           Creative Digital Marketing Agency is a leading and reliable digital marketing agency based in India offering outstanding digital solutions to its clients through SEO, SEM, SMM, PPC, Social Marketing, Web Designing and WordPress Development Services.
         </p>
