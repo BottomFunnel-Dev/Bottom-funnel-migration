22c8e03e2d2e526e4dfab41e65ed62d1494327fb
jyotiranjan98 <jyoti.das955@gmail.com>
2023-02-14 15:44:14 +0530

    added education

---
+++ pages/educationp/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import EducationBanner from '../../components/Educationp/EducationBanner'
+
+export default function Education() {
+  return (
+    <div>
+        <EducationBanner/>
+        
+    </div>
+  )
+}
