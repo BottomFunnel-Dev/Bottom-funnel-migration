68343347c7bc4e9b0651a4de3ee53b20fb215f97
Akshay-Kumar2000 <akshaykumar809177@gmail.com>
2023-02-15 16:35:46 +0530

    changes

--- components/Navbar/NavIndustries.jsx
+++ components/Navbar/NavIndustries.jsx
@@ -4,7 +4,6 @@ import industry from "./navIndustries.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import { industries, ondemand } from "../../Data/Navbar";
-import Link from "next/link";
 export const NavIndustries = ({
   solHoverIn,
   solHoverOut,
