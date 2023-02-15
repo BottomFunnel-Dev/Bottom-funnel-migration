08c9e06acd24efcb8e511e2107b88bcb408a7883
1avinash2023 <118244986+1avinash2023@users.noreply.github.com>
2023-02-15 11:14:01 +0530

    added

--- Data/ApplyAsTalent.js
+++ Data/ApplyAsTalent.js
@@ -1,92 +1,294 @@
-
-
 export const jobroles = [
     {
         title: "PHP Developer",
-        path:"path"
-},
+        path: "path"
+    },
     {
         title: "Paid Search Analyst",
-        path:"path"
-},
+        path: "path"
+    },
     {
         title: "Performance Marketing Analyst",
-        path:"path"
-},
+        path: "path"
+    },
     {
         title: "Programmatic Analyst",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "Salesforce Developer",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "Shopify Developer",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "Android Developer",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "iOS Developer",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "Sr. Salesforce Administrator",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "SEO Analyst",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "QA/Test Engineer",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "SFMC Campaign Manager",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "SFMC Campaign Specialist",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "Python Developer",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "Dot Net Developer",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "DevOps Engineer",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "Java Developer",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "Data Scientist",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "Data Engineer",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "Cloud Engineer",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "Angular JS Developer",
-        path:"path"
-},
-    {
+        path: "path"
+    }, {
         title: "Motion Designer",
-        path:"path"
-},
+        path: "path"
+    },
+]
+
+
+export const currentopeningdata = [
+    {
+        tittle: "Development",
+        content: [
+            {
+                jobreq: "PHP Development",
+                skills: [
+
+                    {
+                        devsk: "Excellent writing skills in English"
+                    },
+                    {
+                        devsk: "Strong research skills"
+                    },
+                    {
+                        devsk: "Expertise in writing about digital marketing."
+                    },
+                    {
+                        devsk: "Editorial mindset with an ability to predict audience preferences."
+                    }, {
+                        devsk: "Hands-on Experience and other online tools"
+                    }
+
+                ]
+            },
+            {
+                jobreq: "Java Development",
+                skills: [
+
+                    {
+                        devsk: "Excellent writing skills in English"
+                    },
+                    {
+                        devsk: "Strong research skills"
+                    },
+                    {
+                        devsk: "Expertise in writing about digital marketing."
+                    },
+                    {
+                        devsk: "Editorial mindset with an ability to predict audience preferences."
+                    }, {
+                        devsk: "Hands-on Experience and other online tools"
+                    }
+
+                ]
+            },
+            {
+                jobreq: "React Development",
+                skills: [
+
+                    {
+                        devsk: "Excellent writing skills in English"
+                    },
+                    {
+                        devsk: "Strong research skills"
+                    },
+                    {
+                        devsk: "Expertise in writing about digital marketing."
+                    },
+                    {
+                        devsk: "Editorial mindset with an ability to predict audience preferences."
+                    }, {
+                        devsk: "Hands-on Experience and other online tools"
+                    }
+
+                ]
+            },
+
+
+        ]
+
+
+    }, 
+    
+    
+    {
+        tittle: "Marketing",
+        content: [
+            {
+                jobreq: "Digital Technology ",
+                skills: [
+
+                    {
+                        devsk: "Excellent writing skills in English"
+                    },
+                    {
+                        devsk: "Strong research skills"
+                    },
+                    {
+                        devsk: "Expertise in writing about digital marketing."
+                    },
+                    {
+                        devsk: "Editorial mindset with an ability to predict audience preferences."
+                    }, {
+                        devsk: "Hands-on Experience and other online tools"
+                    }
+
+                ]
+            }, 
+            {
+                jobreq: "SMM ",
+                skills: [
+
+                    {
+                        devsk: "Excellent writing skills in English"
+                    },
+                    {
+                        devsk: "Strong research skills"
+                    },
+                    {
+                        devsk: "Expertise in writing about digital marketing."
+                    },
+                    {
+                        devsk: "Editorial mindset with an ability to predict audience preferences."
+                    }, {
+                        devsk: "Hands-on Experience and other online tools"
+                    }
+
+                ]
+            }, 
+            {
+                jobreq: "PPC ",
+                skills: [
+
+                    {
+                        devsk: "Excellent writing skills in English"
+                    },
+                    {
+                        devsk: "Strong research skills"
+                    },
+                    {
+                        devsk: "Expertise in writing about digital marketing."
+                    },
+                    {
+                        devsk: "Editorial mindset with an ability to predict audience preferences."
+                    }, {
+                        devsk: "Hands-on Experience and other online tools"
+                    }
+
+                ]
+            }, 
+
+        ]
+
+
+    },
+    
+    {
+        tittle: "Designer",
+        content: [
+            {
+                jobreq: "UI UX",
+                skills: [
+
+                    {
+                        devsk: "Excellent writing skills in English"
+                    },
+                    {
+                        devsk: "Strong research skills"
+                    },
+                    {
+                        devsk: "Expertise in writing about digital marketing."
+                    },
+                    {
+                        devsk: "Editorial mindset with an ability to predict audience preferences."
+                    }, {
+                        devsk: "Hands-on Experience and other online tools"
+                    }
+
+                ]
+            }, 
+            {
+                jobreq: "Graphic",
+                skills: [
+
+                    {
+                        devsk: "Excellent writing skills in English"
+                    },
+                    {
+                        devsk: "Strong research skills"
+                    },
+                    {
+                        devsk: "Expertise in writing about digital marketing."
+                    },
+                    {
+                        devsk: "Editorial mindset with an ability to predict audience preferences."
+                    }, {
+                        devsk: "Hands-on Experience and other online tools"
+                    }
+
+                ]
+            }, 
+            {
+                jobreq: "Adobe",
+                skills: [
+
+                    {
+                        devsk: "Excellent writing skills in English"
+                    },
+                    {
+                        devsk: "Strong research skills"
+                    },
+                    {
+                        devsk: "Expertise in writing about digital marketing."
+                    },
+                    {
+                        devsk: "Editorial mindset with an ability to predict audience preferences."
+                    }, {
+                        devsk: "Hands-on Experience and other online tools"
+                    }
+
+                ]
+            }, 
+
+
+        ]
+
+
+    },
 ]
---
+++ components/ApplyasTalent/Applytalentlowerbanner/Lowerbanner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from "./Lowerbanner.module.css"
+
+
+export const Lowerbanner = () => {
+  return (
+    <div className={styles.hiretalentlowerbannermainboxes}>
+    <div className={styles.hiretalentlowerbannersometextbox}>
+    <h2>Register Your Interest</h2>
+    <p>Currently,we have openings only for the given position but we are in process if adding more skills.
+    If you have a skillset plus experience in other field like SEO, Digital Marketing, Designing and wish to join Uplers, Let us know.
+    Mention your field of interest and we'll make sure you hear back from us once we have openings for the same.
+    </p>
+    <button className={styles.lowerbannerbutton}>Register</button>
+    </div>
+    
+    </div>
+  )
+}
