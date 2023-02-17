74f6062b84c5aed44e6f700166d59ab4a4c36f26
mayra111 <micgupta01@gmail.com>
2023-02-17 19:32:47 +0530

    completedweb dev

--- components/WebDevelopment/bannerWebdev/WebdevBanner.jsx
+++ components/WebDevelopment/bannerWebdev/WebdevBanner.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import { Box, Typography, Modal } from "@mui/material";
+import React, { useState } from "react";
+import { PopupForm } from "../../PopupForm/PopupForm";
 import styles from "./webdevBanner.module.css";
 import { Player } from "@lottiefiles/react-lottie-player";
 
 export default function WebdevBanner({ paraColor, bannerContent, animateData }) {
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      width: 700,
+      height: 550,
+      bgcolor: "background.paper",
+      border: "2px solid #000",
+      // boxShadow: 24,
+      backgorund: "green",
+      p: 1,
+      borderRadius: 2,
+    };
   return (
     <div
       className={styles.webDevBanner}
@@ -22,7 +43,21 @@ export default function WebdevBanner({ paraColor, bannerContent, animateData }) 
           {bannerContent.description}
         </p>
 
-        <button className={styles.webDevButton}>Request A Quote</button>
+        <button className={styles.webDevButton} onClick={() => handleOpen()}>Request A Quote</button>
+        <Modal
+        open={open}
+        onClose={() => handleClose()}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            <div className={styles.popform}>
+              <PopupForm />
+            </div>
+          </Typography>
+        </Box>
+      </Modal>
       </div>
       
       <div className={styles.mernanimation}>
