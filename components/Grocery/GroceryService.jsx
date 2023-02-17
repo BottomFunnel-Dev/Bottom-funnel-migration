f7774eb06f2d978164149cedf6f4583ec32c8d75
Akshay-Kumar2000 <akshaykumar809177@gmail.com>
2023-02-17 17:56:46 +0530

    ondemand session updation

--- components/Grocery/GroceryService.jsx
+++ components/Grocery/GroceryService.jsx
@@ -3,7 +3,7 @@ import styles from "./GroceryService.module.css";
 import Image from "next/image";
 
 export const GroceryService = () => {
-    const [imageChange, setImageChange] = useState("/Images/icecreamchanges/driver app/Profile.png")
+    const [imageChange, setImageChange] = useState("/Images/grocery/Service professionals app/Track My Order.png")
     const handleImageChange = (imagepath) => {
         setImageChange(() => imagepath)
     }
@@ -11,24 +11,28 @@ export const GroceryService = () => {
     const groceryservicedata = [
         {
             image: "/Images/grocery/Service professionals app/Track My Order.png",
+            icon: "/Images/grocery/icons/DriverApp/track my orders.png",
             headertext: "Track My Order",
             paragraph: "In Contrast,manual order tracking system rely on human input and are typically seen in smaller or mid-sized business that handle fewer orders each day.At the most basic level,this often involves manually entering each new order into a database so that it can be tracked throughout the fulfilment process.",
         },
 
         {
             image: "/Images/grocery/Service professionals app/Manage My Cart.png",
+            icon: "/Images/grocery/icons/DriverApp/manage cart.png",
             headertext: "Manage My Cart",
             paragraph: "Our users can work on multiple orders simultaneously via cart Management.You may need to leave a draft order until further clarification or documentation is acquired or you may support several different offices with different procurement needs.",
         },
 
         {
             image: "/Images/grocery/Service professionals app/Simplicity In Searching.png",
+            icon: "/Images/grocery/icons/DriverApp/simplicity in searching.png",
             headertext: "Simplicity In Searching",
             paragraph: "An alternative is to ask whether we can search through the dynamics of natural behavior to find explicit evidence that these behaviors are simpler than they might have been.",
         },
 
         {
             image: "/Images/grocery/Service professionals app/Various Filters.png",
+            icon: "/Images/grocery/icons/DriverApp/various filters.png",
             headertext: "Various Filters",
             paragraph: "As online stores grow and get more sophisticated new ways of guiding customers need to be added.A simple product filter might not be enough.In these cases faceted navigation can help.faceted navigation looks kind of like a filter system."
         },
@@ -60,6 +64,17 @@ export const GroceryService = () => {
                                     handleImageChange(e.image)
                                 }}
                             >
+
+                                <div className={styles.groceruyCustomerappindivisualboxforlogo}>
+                                    <Image src={e.icon}
+                                        alt={"image"}
+                                        width={"0"}
+                                        height={"0"}
+                                        sizes={"100vw"}
+                                        style={{ width: "100%", height: "100%" }}
+                                    />
+                                </div>
+
                                 <div className={styles.groceryserviceindivisualboxfortext}>
                                     <h4> {e.headertext} </h4>
                                     <p> {e.paragraph} </p>
--- components/PickupDelivery/PickupCustomer.jsx
+++ components/PickupDelivery/PickupCustomer.jsx
@@ -3,44 +3,44 @@ import React, { useState } from "react";
 import styles from "./pickupcustomer.module.css";
 export const PickupCustomer = () => {
 
-  const [imageChange, setImageChange] = useState("/Images/pickup/customerapp/easyappboarding.png")
+  const [imageChange, setImageChange] = useState("/Images/pickup/switchscreens/Easy app boarding.webp")
   const handleImageChange = (imagepath) => {
     setImageChange(() => imagepath)
   }
 
   const pickupcustomer = [
     {
-      image: "/Images/pickup/customerapp/easyappboarding.png",
+      image: "/Images/pickup/switchscreens/Easy app boarding.webp",
       pickupicon: "/Images/pickup/icons/customisedapp.png",
       title: "Easy App Onboarding",
       description: "Bottom Funnel is a powerful tool that allows you to identify the strengths and weaknesses of your app.",
     },
 
     {
-      image: "/Images/pickup/customerapp/orderservice.png",
+      image: "/Images/pickup/switchscreens/Order service.webp",
       pickupicon: "/Images/pickup/icons/customersupport.png",
       title: "Order Services",
       description: "It's as easy as ordering from your favorite restaurant or asking a friend to pick up some groceries on the way home.",
     },
 
     {
-      image: "/Images/pickup/customerapp/easyappboarding.png",
+      image: "/Images/pickup/switchscreens/Track order.webp",
       pickupicon: "/Images/pickup/icons/pickupdelivery.png",
       title: "Track Order",
       description: "With Delivery, you can have your orders delivered right to your doorstep by a fleet of autonomous cars or scooters.",
     },
     
     {
-      image: "/Images/pickup/customerapp/trackorder.png",
+      image: "/Images/pickup/switchscreens/in app navigation.webp",
       pickupicon: "/Images/pickup/icons/trackingapp.png",
       title: "In-app Navigation",
       description: "Using In-app navigation, they can find the shortest possible route and reach the customers to hand over the package.",
     },
 
     {
-      image: "/Images/pickup/customerapp/multiplepaymentoption.png",
+      image: "/Images/pickup/switchscreens/Offer & discount.webp",
       pickupicon: "/Images/pickup/icons/analysisdashboard.png",
-      title: "Companies Growth",
+      title: "Offers and Discounts",
       description: "Most companies offer a limited number of options for scheduling pickup and delivery services. Branding yourself as one of the best.",
     },
   ];
