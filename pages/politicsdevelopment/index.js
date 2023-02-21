48c54562a943d080af2c94928959e45dc7462f1f
1avinash2023 <118244986+1avinash2023@users.noreply.github.com>
2023-02-21 10:51:29 +0530

    changes

--- pages/politicsdevelopment/index.js
+++ pages/politicsdevelopment/index.js
@@ -1,13 +1,12 @@
 import React from "react";
+
+import Head from "next/head";
 import { PoliticsAppDevelopmentbanner } from "../../components/PoliticsAppDevelopment/PoliticsAppDevelopmentbanner";
-import { Politicsadminpannel } from "../../components/PoliticsAppDevelopment/Politicsadminpannel";
 import { PoliticsApptextbox } from "../../components/PoliticsAppDevelopment/PoliticsApptextbox";
 import { Politicscustomerapp } from "../../components/PoliticsAppDevelopment/Politicscustomerapp";
+import { Politicsadminpannel } from "../../components/PoliticsAppDevelopment/Politicsadminpannel";
 import { PoliticsDevelopmentlowerbanner } from "../../components/PoliticsAppDevelopment/PoliticsDevelopmentlowerbanner";
-import PoliticsnewsLogo from "../../components/PoliticsAppDevelopment/PoliticsDevelopmentnews";
 import { PoliticsFramework } from "../../components/PoliticsAppDevelopment/PoliticsFramework";
-import { Politicsnewsheading } from "../../components/PoliticsAppDevelopment/Politicsnewsheading";
-import Head from "next/head";
 
 export default function PoliticsAppDevelopment() {
   return (
