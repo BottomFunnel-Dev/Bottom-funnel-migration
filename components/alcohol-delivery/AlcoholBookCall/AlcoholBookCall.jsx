4a5b0b40284a0e70130021d08ec2b282ff9a4987
Jyotiranjan Das <96143764+jyotiranjan98@users.noreply.github.com>
2023-02-24 19:16:05 +0530

    changes

--- components/alcohol-delivery/AlcoholBookCall/AlcoholBookCall.jsx
+++ components/alcohol-delivery/AlcoholBookCall/AlcoholBookCall.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { PopupForm } from "../../PopupForm/PopupForm";
 import styles from "./AlcoholBookCall.module.css";
+
 
 export default function AlcoholBookCall() {
   return (
@@ -9,9 +11,40 @@ export default function AlcoholBookCall() {
           Get all the alcohol delivery app development solutions for all the kind of platform?
         </h1>
         
-        <button>
+        <button 
+        type="button"
+        data-bs-toggle="modal"
+        data-bs-target="#staticBackdrop"
+        >
           Let's Get Started
         </button>
+
+        <div
+            className="modal fade"
+            id="staticBackdrop"
+            data-bs-backdrop="static"
+            data-bs-keyboard="false"
+            aria-labelledby="staticBackdropLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+
+                <div>
+                  <PopupForm/>
+                </div>
+              </div>
+            </div>
+          </div>
+
       </div>
     </div>
   );
