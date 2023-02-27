127c465763dacbaf7c72945d777cbec03f1bb78d
1avinash2023 <118244986+1avinash2023@users.noreply.github.com>
2023-02-27 15:02:04 +0530

    changes call to action

--- components/Ott-App-Page/Ott-Features/OttFeatures.jsx
+++ components/Ott-App-Page/Ott-Features/OttFeatures.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { PopupForm } from "../../PopupForm/PopupForm";
 import styles from "./ottFeatures.module.css";
 
 export default function OttFeatures() {
@@ -70,7 +71,38 @@ export default function OttFeatures() {
             What's Special About Online Flower Delivery App Solutions Developed
             At Bottom Funnel
           </h2>
-          <button>Get Started</button>
+          <button
+          type="button"
+          data-bs-toggle="modal"
+          data-bs-target="#staticBackdrop"
+          
+          >Get Started</button>
+
+          <div
+          className="modal fade"
+          id="staticBackdrop"
+          data-bs-backdrop="static"
+          data-bs-keyboard="false"
+          aria-labelledby="staticBackdropLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+
+              <div>
+                <PopupForm />
+              </div>
+            </div>
+          </div>
+        </div>
         </div>
       </div>
     </div>
