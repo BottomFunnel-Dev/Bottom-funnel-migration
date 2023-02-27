c63c4cdeb930ffb303363d9799a8f5a9280ab061
Deepanshu Gulia <deepanshugulia256@gmail.com>
2023-02-27 16:09:45 +0530

    added popup form to all skills page

--- components/SkillsCom/SkillsComBanner/index.js
+++ components/SkillsCom/SkillsComBanner/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import React from "react";
 // import styles from "./SkillsComBanner.module.css";
+import { PopupForm } from "../../PopupForm/PopupForm";
 
 export default function SkillsComBanner({ data }) {
   const { image, title, description, icon, points } = data;
@@ -32,7 +33,38 @@ export default function SkillsComBanner({ data }) {
               <li key={elem}>{elem}</li>
             ))}
           </ul>
-          <button>Get Started</button>
+          <button
+            type="button"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+          >
+            Get Started
+          </button>
+          <div
+            className="modal fade"
+            id="skillsBannerPopupForm"
+            data-bs-backdrop="static"
+            data-bs-keyboard="false"
+            aria-labelledby="staticBackdropLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+
+                <div>
+                  <PopupForm />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <img src={icon} alt={icon} />
       </div>
