7e0e3cfbb1196153963d66c6eaa73e8220f73dd3
1avinash2023 <118244986+1avinash2023@users.noreply.github.com>
2023-02-27 17:25:42 +0530

    changes

--- components/Dating/Datingcontainer.jsx
+++ components/Dating/Datingcontainer.jsx
@@ -12,7 +12,13 @@ export default function Datingcontainer() {
             <p>
             There's no doubt that dating apps have revolutionized the way we date and find love. But with so many different options out there, it can be hard to know which one is right for you. That's why we've created this comprehensive guide to the best dating app solutions out there.
             </p>
-            <button className={styles.szdatingbtn}>Get Started</button>
+            <button className={styles.szdatingbtn}
+            type="button"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+            
+            >Get Started</button>
+            
           </div>
         </div>
         <div className={styles.datinglogodiv}>
--- components/Sportsbetting/SportsBanner.jsx
+++ components/Sportsbetting/SportsBanner.jsx
@@ -24,31 +24,7 @@ export default function SportsBanner() {
           
           >Get Started</button>
 
-          <div
-          className="modal fade"
-          id="staticBackdrop"
-          data-bs-backdrop="static"
-          data-bs-keyboard="false"
-          aria-labelledby="staticBackdropLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                ></button>
-              </div>
-
-              <div>
-                <PopupForm />
-              </div>
-            </div>
-          </div>
-        </div>
+         
         </div>
       </div>
 
