abdff070105240fd15a3f3216449e23ad26835cb
Deepanshu Gulia <deepanshugulia256@gmail.com>
2023-02-28 15:52:25 +0530

    done with all crawled 404 pages and added robots.txt

--- next.config.js
+++ next.config.js
@@ -28,6 +28,101 @@ module.exports = withBundleAnalyzer({
         destination: "/link-building-services",
         permanent: true,
       },
+      {
+        source: "/seo-guest-post",
+        destination: "/guest-posting-services",
+        permanent: true,
+      },
+      {
+        source: "/emailservices",
+        destination: "/emailmarketing-services",
+        permanent: true,
+      },
+      {
+        source: "/iconic",
+        destination: "/ionic",
+        permanent: true,
+      },
+      {
+        source: "/asp-net-development",
+        destination: "/asp.net-development",
+        permanent: true,
+      },
+      {
+        source: "/artificial-inteligence",
+        destination: "/artificial-intelligence",
+        permanent: true,
+      },
+      {
+        source: "/seo",
+        destination: "/seo-services",
+        permanent: true,
+      },
+      {
+        source: "/mean-stack-development",
+        destination: "/meanstack-development",
+        permanent: true,
+      },
+      {
+        source: "/digitalmarketing",
+        destination: "/digital_marketing",
+        permanent: true,
+      },
+      {
+        source: "/Fintech-solution",
+        destination: "/fintech-solution",
+        permanent: true,
+      },
+      {
+        source: "/Social-Media",
+        destination: "/industries/social_media",
+        permanent: true,
+      },
+      {
+        source: "/food-delivery",
+        destination: "/food-delivery-app",
+        permanent: true,
+      },
+      {
+        source: "/reactNative",
+        destination: "/reactnative-development",
+        permanent: true,
+      },
+      {
+        source: "/xamarin-Development",
+        destination: "/xamarin-development",
+        permanent: true,
+      },
+      {
+        source: "/Logistics",
+        destination: "/industries/logistics",
+        permanent: true,
+      },
+      {
+        source: "/beautycareapp",
+        destination: "/beauty-and-salon",
+        permanent: true,
+      },
+      {
+        source: "/ios",
+        destination: "/ios-development",
+        permanent: true,
+      },
+      {
+        source: "/mern-stack",
+        destination: "/mernstack-development",
+        permanent: true,
+      },
+      {
+        source: "/android",
+        destination: "/android-development",
+        permanent: true,
+      },
+      {
+        source: "/flutter",
+        destination: "/flutter-development",
+        permanent: true,
+      },
     ];
   },
 });
