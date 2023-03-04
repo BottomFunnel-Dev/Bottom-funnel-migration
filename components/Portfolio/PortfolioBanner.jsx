943b40cd30d2cfaad4b66a974e3c03c527cd022f
Akshay-Kumar2000 <akshaykumar809177@gmail.com>
2023-03-04 15:49:14 +0530

    portfolio page banner data changes

--- components/Portfolio/PortfolioBanner.jsx
+++ components/Portfolio/PortfolioBanner.jsx
@@ -4,16 +4,16 @@ import styles from "./PortfolioBanner.module.css"
 export const PortfolioBanner = () => {
   return (
     <div className={styles.portfoliobannermainboxes}>
-    <div className={styles.portfoliobannerheadingtxt}>
-    <h1><span>Our</span> <span>Portfolio</span></h1>
-    <p>We aim at delivering rich digital experience a d building meaningful relationships with our clients through outstanding designs,business centered web solutions and best customer support.</p>
-    
-    <div>
-    <button className={styles.portfoliobannerbutton}>Get Started</button>
-    </div>
-    
-    </div>
-   
+      <div className={styles.portfoliobannerheadingtxt}>
+        <h1><span>Our</span> <span>Portfolio</span></h1>
+        <p>We aim at delivering rich digital experience and building meaningful relationships with our clients through outstanding designs,business centered web solutions and best customer support.</p>
+
+        <div>
+          <button className={styles.portfoliobannerbutton}>Get Started</button>
+        </div>
+
+      </div>
+
     </div>
   )
 }
--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,4 @@
 import React, { useEffect } from "react";
-import Image from "next/image";
-import styles from "../styles/404.module.css";
 import { useRouter } from "next/router";
 
 export default function Notfound() {
